488a4e6c81957c0484faeb0af11d80d4a8ce5e34
ioko <ikobozev3@gmail.com>
2024-01-28 14:39:29 +0300

    main.py added

---
+++ main.py
@@ -0,0 +1,24 @@
+#вывести текст в консоль
+print("hello world")
+
+#вывести в консоль несколько значений
+var1 = 'hello,' #одинарные кавычки
+var2 = "world"  #двойные кавычки
+var3 = "again"
+print(var1, var2, var3)
+
+#базовые типы данных Python
+a = 15 #int
+b = 13.141343 #float 
+c = True #bool
+d = "hello" #str
+value = None
+
+#вывод типа данных переменной:
+print(type(a))
+'''print(type(b))
+print(type(c))''' #многострочный комментарий - тройные кавычки
+print(type(d))
+print(type(value))
+
+
